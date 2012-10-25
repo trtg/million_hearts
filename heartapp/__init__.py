4516c78def6d2686dfe8daae70d74a3dc7723ab4
Sebastian Ortiz <sebortiz@gmail.com>
2012-10-25 00:27:23 -0700

    preliminary functionality for million hearts challenge, gathers all data, uses html5 geolocation to get currentlocation, queries surescripts API to get a list of testing centers and puts them on a google map with driving direction links

---
+++ heartapp/__init__.py
@@ -0,0 +1,27 @@
+from flask import Flask,render_template,request,redirect,url_for,abort,session,jsonify
+import requests
+import json
+#this is excluded from repo, place your own 
+#credentials in mashape_auth.py
+#from mashape_auth import auth_header 
+
+app = Flask(__name__,static_path='/static/')
+#never enable this when externally visible
+app.config['DEBUG']=True
+
+@app.route('/')
+def data_input():
+    return render_template('data_input.html')
+
+@app.route('/find_test_centers')
+def find_test_center():
+    #app.logger.error(request.args.get('radius'))
+    #app.logger.error(request.query_string)
+    location_query = 'https://millionhearts.surescripts.net/test/Provider/Find?%s' % request.query_string
+    #params=request.args ?
+    result=requests.get(location_query)
+    app.logger.error(result.json)
+    return jsonify(result.json)
+
+if __name__ == '__main__':
+    app.run()
